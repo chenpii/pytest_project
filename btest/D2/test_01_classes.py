3eafd067673a1e29898f1f9f27db5521f57e7c39
admin <admin@example.com>
2023-04-20 20:42:54 +0800

    面向对象断言

--- btest/D2/test_01_classes.py
+++ btest/D2/test_01_classes.py
@@ -19,3 +19,12 @@ def test_field_access():
 
     with assume:
         assert p.weight == 50
+
+
+class TestEquality():
+    def test_person(self):
+        # 预期值
+        expected_p = Person("lyf", 20, 170, 60)
+        # 实际值
+        actual_p = Person("lyf", 20, 170, 60)
+        assert expected_p == actual_p
